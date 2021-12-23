fc96a2e1d77f9205e3f3de29427d173e38497017
naoyaohzawa <naoya.ohh@gmail.com>
2021-12-23 19:41:25 +0900

    オーナー登録画面

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -25,16 +25,20 @@ class OwnersController extends Controller
         // echo $date_now->year;
         // echo '<br>';
         // echo $date_parse;
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
 
-        $c_test = collect([
-            'name' => 'テストコレクション'
-        ]);
+        // $c_test = collect([
+        //     'name' => 'テストコレクション'
+        // ]);
 
         // dd($e_all, $q_get, $q_first, $c_test);
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+
+        // dd($owners);
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -45,6 +49,7 @@ class OwnersController extends Controller
     public function create()
     {
         //
+        return view('admin.owners.create');
     }
 
     /**
